573eb9cb42fd0060beb2545973333b37eb8d7d47
emz5 <emz5@users.noreply.github.com>
2020-05-03 00:02:06 -0700

    first commit

--- Hello.py
+++ Hello.py
@@ -2,5 +2,10 @@ import random
 
 wordlist = list()
 wordlist = [word.rstrip("\n") for word in open("Hangman_Words.txt")]
-index = random.randint(0, 212)
-print(wordlist[index])
+index = random.randint(0, len(wordlist) - 1)
+number = 1
+while "Emily" != wordlist[index]:
+    print(wordlist[index] + "(", number, ")")
+    index = random.randint(0, len(wordlist) - 1)
+    number = number + 1
+print("Emily has been found")
